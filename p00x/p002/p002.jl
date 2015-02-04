5df24bc12e23cbaf3185809022fb2d760e9d41da
aomoriringo <sel0433@gmail.com>
2015-02-05 08:44:12 +0900

    refactored p002.jl

--- p00x/p002/p002.jl
+++ p00x/p002/p002.jl
@@ -1,14 +1,3 @@
 PHY = (1+sqrt(5))/2
 fib(n) = int(round(PHY^n / sqrt(5)))
-
-fibs = Int[]
-for i=1:1000
-  x = fib(i)
-  if x > 4000000
-    break
-  elseif x%2 == 0
-    continue
-  end
-  push!(fibs,fib(i))
-end
-sum(fibs) |> println
+filter(x -> x<=4000000 && x%2==0, map(x -> fib(x), [1:50])) |> sum |> println
