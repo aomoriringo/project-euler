d8fda85b4bf2a7fbf1ac2417973c3346135758a0
aomoriringo <sel0433@gmail.com>
2015-06-22 06:36:50 +0900

    fix p002.jl

--- p00x/p002/p002.jl
+++ p00x/p002/p002.jl
@@ -1,3 +1,3 @@
 PHI = (1+sqrt(5))/2
 fib(n) = int(round(PHI^n / sqrt(5)))
-filter(x -> x<=4000000 && x%2==0, map(x -> fib(x), [1:50])) |> sum |> println
+filter(x -> x<=4000000 && x%2==0, [fib(x) for x=1:50]) |> sum |> println
